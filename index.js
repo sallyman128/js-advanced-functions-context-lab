1f4a0e2a058c992bc08f0b45792c55447ed63819
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-09 16:48:04 -0400

    commit tree dc39abb77f6abfbaacd8a5e586abbf5651b83a99

--- index.js
+++ index.js
@@ -19,4 +19,65 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+};
+
+const createEmployeeRecord = (employee) => {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+};
+
+const createEmployeeRecords = (employees) => {
+    return employees.map( (employee) => createEmployeeRecord(employee) )
+};
+
+const createTimeInEvent = function(dateIn) {
+    const date = dateIn.slice(0,10);
+    const hour = parseInt(dateIn.slice(11));
+    this.timeInEvents.push({
+        date: date,
+        hour: hour,
+        type: "TimeIn"
+    })
+    return this;
+};
+
+const createTimeOutEvent = function(dateOut) {
+    const date = dateOut.slice(0,10);
+    const hour = parseInt(dateOut.slice(11));
+    this.timeOutEvents.push({
+        date: date,
+        hour: hour,
+        type: "TimeOut"
+    })
+    return this;
+}
+
+const hoursWorkedOnDate = function(targetDate) {
+    const timeInDay = this.timeInEvents.find( (day) => day.date == targetDate);
+    const timeOutDay = this.timeOutEvents.find( (day) => day.date == targetDate);
+    return (timeOutDay.hour - timeInDay.hour ) / 100;
+}
+
+const wagesEarnedOnDate = function(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+const findEmployeeByFirstName = function(employees, targetFirstName) {
+    return employees.find( (employee) => employee.firstName === targetFirstName )
+}
+
+const calculatePayroll = function(employees) {
+    return employees.reduce( (totalExpense, employee) => {
+        // let employeeWorksDays = createEmployeeRecord(employee).timeInEvents;
+        // let totalEmployeeExpense = employeeWorksDays.reduce( (employeeExpense, timeIn) => {
+        //     return employeeExpense + wagesEarnedOnDate.call(employee, timeIn.date);
+        // }, 0)
+        return totalExpense + allWagesFor.call(employee);
+    }, 0)
 }
